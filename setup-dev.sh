1f1ca03168f8f3ae2b4b480646491b931795e1a0
Cyril de Wit <cyrilwit@gmail.com>
2023-12-09 15:55:18 +0100

    feat: extend setup-dev script with Laravel Passport setup

--- setup-dev.sh
+++ setup-dev.sh
@@ -4,27 +4,34 @@ function runScript {
     docker compose exec php "$@"
 }
 
-function initializeProject() {
-    if ! [ -f ".env" ]; then
-        ENVIRONMENT_FILE=".env.local"
-        echo "==[ Copying $ENVIRONMENT_FILE to .env"
-        cp $ENVIRONMENT_FILE .env
+isFirstInstallation=false
 
-        echo "==[ Create storage link"
-        runScript php artisan storage:link
-    fi
-}
+if ! [ -f ".env" ]; then
+    isFirstInstallation=true
+fi
 
 echo "==[ DOCKER COMPOSE UP ]==";
 docker compose up -d --remove-orphans
 
-initializeProject
+if [ "$isFirstInstallation" = true ]; then
+    ENVIRONMENT_FILE=".env.local"
+    echo "==[ Copying $ENVIRONMENT_FILE to .env"
+    cp $ENVIRONMENT_FILE .env
+fi
 
 echo "==[ Install composer dependencies"
 runScript composer install
 
 echo  "==[ Run database migrations"
 runScript php artisan migrate
+
+if [ "$isFirstInstallation" = true ]; then
+    echo "==[ Create storage link"
+    runScript php artisan storage:link
+
+    echo "==[ Installing Laravel Passport"
+    runScript php artisan passport:install
+fi
 
 echo  "==[ Opening shell in PHP service"
 docker compose exec php sh
