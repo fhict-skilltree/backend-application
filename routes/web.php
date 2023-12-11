34aeee7c375ceab15d3b55c6f70714c2be5a6c25
Cyril de Wit <cyrilwit@gmail.com>
2023-12-11 21:36:10 +0100

    feat: replace test endpoint with simple ok response

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->check()) {
-        dump(auth()->user()->first_name);
-    }
-
-    return response('success');
+    return response('ok');
 });
 
 Route::get('login', function () {
