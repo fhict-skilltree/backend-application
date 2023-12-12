1e4b33b872e2764b0f853cc6ec5444e852915995
Cyril de Wit <cyrilwit@gmail.com>
2023-12-12 19:28:43 +0100

    feat: seed local oauth clients by default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,11 +8,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
+        $this->call(OAuthClientSeeder::class);
         $this->call(FontysHogeschoolSeeder::class);
     }
 }
