bb63627a5a11b5c572c7dc732a54981908ace804
Cyril de Wit <cyrilwit@gmail.com>
2023-12-12 19:29:57 +0100

    feat: create Course API resource

---
+++ app/App/Courses/Http/Resources/CourseResource.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Courses\Http\Resources;
+
+use Domain\Courses\Models\Course;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes as OA;
+
+/**
+ * @mixin Course
+ */
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    #[OA\Schema(
+        schema: 'CourseResource',
+        properties: [
+            new OA\Property(
+                property: 'uuid',
+                description: 'The uuid of the course',
+                type: 'integer'
+            ),
+            new OA\Property(
+                property: 'title',
+                description: 'The title the user',
+                type: 'string'
+            ),
+            new OA\Property(
+                property: 'content',
+                description: 'The content of the course',
+                type: 'string'
+            ),
+            new OA\Property(
+                property: 'created_at',
+                description: 'The date the course is created',
+                type: 'string',
+                format: 'date-time',
+            ),
+            new OA\Property(
+                property: 'updated_at',
+                description: 'The date the course is updated',
+                type: 'string',
+                format: 'date-time',
+            ),
+        ],
+        type: 'object',
+    )]
+    public function toArray(Request $request): array
+    {
+        return [
+            'uuid' => $this->uuid,
+            'title' => $this->title,
+            'content' => $this->content,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
