c445c49b3b4d710906718d34ec9d9601ee8caa71
Cyril de Wit <cyrilwit@gmail.com>
2023-12-12 19:04:19 +0100

    feat: create basic courses table

---
+++ database/migrations/2023_12_10_213621_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Domain\Courses\Models\Course;
+use Domain\Users\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('course_user_enrollments', function (Blueprint $table) {
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Course::class);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+        Schema::dropIfExists('course_user_enrollments');
+    }
+};
