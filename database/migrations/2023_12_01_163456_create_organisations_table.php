1cd7ceeeda7fccff96f4efdeaa3561846f3ac6b0
Cyril de Wit <cyrilwit@gmail.com>
2023-12-12 19:04:01 +0100

    feat: add uuid to tables which can be used as a reference by consumers of our REST API

--- database/migrations/2023_12_01_163456_create_organisations_table.php
+++ database/migrations/2023_12_01_163456_create_organisations_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     {
         Schema::create('organisations', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('name');
             $table->timestamps();
         });
