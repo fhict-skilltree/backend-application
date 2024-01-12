eb3c1b9a329c12c08fa08b2d26bfbfb6c29dfb37
Cyril de Wit <cyrilwit@gmail.com>
2024-01-12 16:05:32 +0100

    feat: add start-build shell script

---
+++ start-build.sh
@@ -0,0 +1,9 @@
+function dockerCompose {
+    docker compose -f docker-compose.build.yml "$@";
+}
+
+# Down the stack
+docker compose down --remove-orphans
+
+echo "==[ DOCKER BUILD AND START CONTAINERS ]==";
+dockerCompose up  -d --remove-orphans --build
